8058db40f17142ae6eed8b75aacfb6e123bd1a6b
Iran <iran.montielbuap@gmail.com>
2023-01-17 15:02:29 -0600

    Agregue cambios referentes a JS basico

---
+++ scripts/funciones.js
@@ -0,0 +1,14 @@
+function saludar() {
+    console.log("Hola usuario! Como estas?");
+}
+saludar();
+
+function calculo1 (sum1, sum2, sum3) {
+    let resultado = sum1 + sum2 + sum3;
+    console.log(resultado)
+}
+calculo1 (2, 3, 8);
+
+function calculo2 (a, b) {
+
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,49 @@
-let nombre = "Tibu"
-const edad = 19
-console.log (nombre);
-console.log (edad);
-nombre = "Miauricio";
+/* 
+    Semana5: JS Basico
+    Autor: Iran Rendon Montiel
+    Fecha: 17/01/2023
+*/
 
-console.log (nombre);
+// Variable - String
+var saludo = "Hola mundo";
+
+if(saludo){
+    var saludo = "Hola Puebla";
+    console.log(saludo);
+}
+
+let nombre = "Victoria";
+if(nombre){
+    let nombre = "Pedro";
+    console.log(nombre);
+}
+
+// Variable Number
+let edad = 35;
+
+// Variable Boleana
+let Pato = true;
+let falso = false;
+
+// Variable Indefinido
+let Gato = undefined;
+
+// Variable Null
+let nulo = null
+
+// Interpolacion de texto
+console.log (`Hola Saturno, mi nombre es ${nombre} y tengo ${edad} años de edad\n`);
+console.log (`Magia, ahora los ${Pato} no son lo mismo que los ${Gato}\n`);
+
+// Variables Number
+let a = 4
+let b = 6
+let x = 2
+
+console.log(`La suma de A y B es: ${a+b}\n`)
+
+let sumaResultado = a+b
+
+if(sumaResultado>x){
+    console.log(`La suma de A+B = ${sumaResultado} es mayor que X = ${x}`)
+}
